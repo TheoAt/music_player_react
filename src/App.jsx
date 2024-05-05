9b4fc48c070ba38b391dd3e30ae9da2d2ec9ca91
TheoAt <theo.atlani@gmail.com>
2024-05-05 19:44:18 +0200

    initialisation and starting project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MusicPlayer from './components/MusicPlayer';
+
+function App() {
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Music Player</h1>
+      <MusicPlayer />
+    </div>
+  );
+}
+
+export default App;
